f08487d0c9da15ccffc8b77305575e708f1b252d
Max Wells <maxwells.pro@proton.me>
2024-12-11 17:13:22 +0100

    commit

---
+++ README.md
@@ -0,0 +1,7 @@
+To run this project :
+
+```bash
+cargo leptos watch
+```
+
+![GitHub Rust Toast App](github-rust-toast-app.gif)
---
+++ src/components/mod.rs
@@ -0,0 +1,12 @@
+pub mod icons;
+pub mod toaster_custom;
+pub mod ui;
+
+pub mod navbar;
+pub mod reactive_indicator;
+pub mod tailwind_indicator;
+pub mod theme_toggle;
+
+// DEMOS
+pub mod demo_toast;
+pub mod demo_toast_variants;
